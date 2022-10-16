86855cef1e1a459889b359d81b1f86e98ae74da4
Guest <guest-cq86q3@h27706.sgb.skir.kr>
2022-10-16 14:37:25 +0900

    3

--- 2928.cpp
+++ 2928.cpp
@@ -19,12 +19,13 @@ typedef struct node
 };
 
 int n,m;
+int x[4];
 node seg[2700010];
+int ten=1000000;
 
 long long calc (int idx)
 {
     long long result = seg[idx].n*seg[idx].var;
-    result += idx*seg[idx].tvar;
     result += seg[idx].n*(seg[idx].n+1)/ 2 * seg[idx].tvar;
     return result;
 }
@@ -32,8 +33,8 @@ long long calc (int idx)
 long long query(int start,int end)
 {
     long long result = 0;
-    start += 1000000;
-    end += 1000000;
+    start += ten;
+    end += ten;
     for (; start<end; start/=2, end/=2)
     {
         if(start%2) result+=calc(start++);
@@ -43,64 +44,154 @@ long long query(int start,int end)
     return result;
 }
 
-void update(int start, int end, long long var, long long tvar)
+long long update(int start, int end, node data)
 {
     start=abs(start);
     end=abs(end);
-    start += 100000;
-    end += 100000;
-    node data;
-    data.var=var;
-    data.tvar=tvar;
+    start += ten;
+    end += ten;
+    int n = 1;
     for (; start<end; start/=2, end/=2)
     {
+        data.n=n;
         if(start%2) seg[start++] += data;
         if (!(end%2)) seg[end--] += data;
+        n*=2;
     }
     if (start==end) seg[start] += data;
+    n = end - start + 1;
+    return n*data.var + n*(n+1)/2 * data.tvar;
+}
+node getval(int t)
+{
+    node result;
+    if (x[0] > t || x[1] > t|| x[2] < -t|| x[3] < -t) return result;
+    if (x[0] > -t)
+    {
+        if (x[1]>-t)
+        {
+            if(x[2]<t)
+            {
+                if(x[3]<t);
+                else result.var = x[2]-x[0]+1;
+            }
+            else
+            {
+                if(x[3]<t) result.var=x[3]-x[1]+1;
+                else 
+                {
+                    result.tvar = 2;
+                    result.var = x[2]+ t + t - x[1] + 1;
+                }
+            }
+        }
+        else
+        {
+            if(x[2]<t)
+            {
+                if(x[3]<t) result.var = x[2]-x[0]+1;
+                else result.var = 2*(x[2]-x[0]+1);
+            }
+            else
+            {
+                if(x[3]<t)
+                {
+                    result.tvar = 2;
+                    result.var = t - x[0] + x[3] + t + 1;
+                }
+                else 
+                {
+                    result.tvar = 4;
+                    result.var = 2*(t-x[0]) + t + t + 1;
+                }
+            }
+        }
+    }
+    else
+    {
+        if (x[1]>-t)
+        {
+            if(x[2]<t)
+            {
+                if(x[3]<t) result.var = x[3]-x[1]+1;
+                else
+                {
+                    result.tvar = 2;
+                    result.var = x[2]+t + t - x[0] + 1;
+                }
+            }
+            else
+            {
+                if(x[3]<t) result.var= x[2]+t + t - x[0] + 1;
+                else 
+                {
+                    result.tvar = 4;
+                    result.var = 2*(t-x[1]) + t + t  + 1;
+                }
+            }
+        }
+        else
+        {
+            if(x[2]<t)
+            {
+                if(x[3]<t) 
+                {
+                    result.tvar = 2;
+                    result.var = x[2]+t + x[3] + t + 1;
+                }
+                else 
+                {
+                    result.tvar = 4;
+                    result.var = 2*(x[2]+t) + t + t  + 1;
+                }
+            }
+            else
+            {
+                if(x[3]<t)
+                {
+                    result.tvar = 4;
+                    result.var = 2*(x[3]+t) + t + t  + 1;
+                }
+                else 
+                {
+                    result.tvar = 8;
+                    result.var = 4*t-4;
+                }
+            }
+        }
+    }
+    return result;
 }
 
+void getnode(int t,long long a)
+{
+    node result;
+    result.var = (min(x[2],t) - max(x[0],-t) +1)*(min(x[3],t)-max(x[1],-t)+1)-a;
+    seg[ten+t] += result;
+}
 int main(void)
 {
     cin >> n;
     int x1,x2,y1,y2;
-    int x[4];
-    vector<pair<int,int>> temp;
+    vector<int> temp;
     int cap;
-    int ten=1000000;
+    node data;
     for (cap=1;cap<ten;cap*=2);
     for(int i = cap+ten;i<cap*2;i++) seg[i].n = 0;
     for(int i = cap -1 ; i>0 ; i--) seg[i].n = seg[i*2].n+seg[i*2+1].n;
     for (int i=0;i<n;i++)
     {
-        int count = 0;
+        long long count = 0;
         cin >> x[0] >> x[1] >> x[2] >> x[3]; //x1 y1 x2 y2 
-        for (int j=-1;j<2;j=j+2) for (int k=0;k<4;k++) temp.push_back(make_pair(j*x[k],k%2));
-        if (temp[0].first==x[0] || temp[0].first == x[1] || temp[0].first == -x[2] || temp[0].first == -x[3]) continue;
-        update(0,temp[0].first,4,8);
-        for (int k=1;k<4;k++)
+        for (int k=0;k<4;k++) temp.push_back(abs(x[k]));
+        sort(temp.begin(),temp.end());
+        getnode(0,count);
+        count+=update(1,temp[0]-1,getval(1));
+        for (i=0;i<3;i++)
         {
-            if (temp[k].first==x[0] || temp[k].first == x[1] || temp[k].first == -x[2] || temp[k].first == -x[3]) break;
-            else
-            {
-                switch (count){
-                        case 0 : update(temp[0].first,temp[1].first,2-2*temp[0].first,4); //맞나 싶다 너무 x1기준인듯?
-                                        break;
-                        case 1 : if(temp[0].second == temp[1].second) 
-                                        {
-                                            if (temp[0].second)update(temp[1].first,temp[2].first,2*(x[3]-x[1]),0);
-                                            else update(temp[1].first,temp[2].first,2*(x[2]-x[1]),0);
-                                        }
-                                        else update(temp[1].first,temp[2].first,temp[0].first+temp[1].first+1,2);
-                                        break;
-                        case 2 : if(temp[0].second+temp[1].second+temp[2].second == 2)update(temp[2].first,temp[3].first,x[3]-x[1],0);
-                                        else update(temp[2].first,temp[3].first,x[2]-x[0],0);
-                                        break;
-                        default : break;
-                }
-                count++;
-            }            
-        }
+            getnode(temp[i],count);
+            count += update(temp[i]+1,temp[i+1]-1,getval(temp[i]));
+        } 
     }
     cin >> m;
     int last_t = 0;
